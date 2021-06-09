03fc7bc7e723b4c4e447c3bf0c928cce8959f7da
Kacper <kacpergierycz@gmail.com>
2021-06-09 17:09:56 +0200

    login servlet doPost parameters usreID, password

--- LoginApp/WebContent/Login.jsp
+++ LoginApp/WebContent/Login.jsp
@@ -8,7 +8,7 @@
 </head>
 <body>
 
-<form action="" method="post"></form>
+<form action="login" method="post"></form>
 	<br>User ID: <input type="text" name="userId">
 	<br>Password: <input type="password" name="password">
 	<br><input type="submit"/>
--- LoginApp/src/Login/LoginServlet.java
+++ LoginApp/src/Login/LoginServlet.java
@@ -13,14 +13,16 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/login")
 public class LoginServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		String userId, password;
+		
+		userId=request.getParameter("userId");
+		password=request.getParameter("password");
+		
 	}
 
 }
